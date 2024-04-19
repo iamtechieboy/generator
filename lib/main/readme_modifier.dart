01baf002b4e0f7c72f1a514bce79bfe71537ed2b
Abuvakhobov Isroiljon <65494864+Isroiljoncyber@users.noreply.github.com>
2024-04-19 10:55:36 +0500

    fix: some changes

--- lib/main/readme_modifier.dart
+++ lib/main/readme_modifier.dart
@@ -27,10 +27,19 @@ Project is ready to modify and add new features.
   -Before running the project, add fonts you want to use in the project to the assets/fonts folder and update pubspec.yaml file.
   -As we are still working on this project, Swagger UI models might be generated wrongly, please check the actual swagger model
 
-Here some other essential commands to help you: 
+Here some other essential commands to help you:
+
+-- Generate CLA project =>
+  \$ dart compile exe bin/main.dart -o bin/generator 
+     after generating the executable file, make sure to add the path to the environment variable
+     on macOS register path of generator file in shell profile e.g. ~/.zshrc or ~/.bash_profile
+     on windows register path of generator file in environment variables check it out => https://phoenixnap.com/kb/windows-set-environment-variable
+
+-- Generate new project =>
+  \$ generator project
 
 -- Generate new feature =>
-  \$ generate feature
+  \$ generator feature
 
 - Regenerate LocaleKeys =>
   \$ dart run easy_localization:generate -S "assets/translations" -O "lib/generated" & dart run easy_localization:generate -S "assets/translations" -O "lib/generated" -o "locale_keys.g.dart" -f keys
